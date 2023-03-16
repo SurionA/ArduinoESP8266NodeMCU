4fd9d2d1a7e133533a98c36b0b2045c614a928a7
Maxime FRERE <maxime.frere@timeonegroup.com>
2023-03-16 21:42:27 +0100

    feat: send data to server on wakeup

--- src/main.cpp
+++ src/main.cpp
@@ -3,59 +3,54 @@
 #include "HC_WiFi.h"
 #include "HC_ntp.h"
 #include "HC_DHTSensors.h"
-#include "HC_Server.h"
+// #include "HC_Server.h"
+#include "HC_http.h"
+
+#include "HC_utils.h"
 #include "secrets.h"
 
-// int sleepInterval = 2;
-int jobDone = 0;
+int sleepInterval = 2;
 
 void setup() {
   Serial.begin(9600);
   Serial.setDebugOutput(true);
 
-
-  pinMode(D7, OUTPUT);
-  digitalWrite(D7, HIGH);
-  pinMode(D8, OUTPUT);
-  digitalWrite(D8, HIGH);
-
   HC_WiFi::connect((const char*) SECRETS::ssid, (const char*) SECRETS::password, SECRETS::hostname);
-
   HC_ntp::init("CET-1CEST,M3.5.0,M10.5.0/3");
+  HC_DHTSensors.setup(D1, DHTesp::DHT22);
+  HC_http.init(SECRETS::server, SECRETS::port);
 
-  HC_DHTSensors.setup(D0, DHTesp::DHT11);
-
-  HC_Server::init();
-  HC_Server::discoveryService();
-  HC_ntp::printLocalTime();
+  // HC_Server::init();
+  // HC_Server::discoveryService();
 }
-using Callback = void (*)();
-
-void loopSleep(Callback callback, int sleepInterval ) {
-  int minutes = HC_ntp::getMinutes();
-
-  if (fmod(minutes, sleepInterval) == 0 && jobDone ==0) {
-    jobDone=1;
-    callback();
-  } else {
-    jobDone=0;
-
-    int sleepDelay = HC_ntp::getDelaySyncTime(sleepInterval);
-    Serial.println();
-    Serial.println("Estimated sleep: " +  (String) sleepDelay);
-
-    delay(sleepDelay);
-  }
-}
-
 
 void callback() {
-  HC_DHTSensors.updateHcValues();
   Serial.println();
   HC_ntp::printLocalTime();
-  Serial.println("JOB IS DONE !!");
+  HC_DHTSensors.updateHcValues();
+  HC_DHTSensors.displayTempHumidity();
+  HC_http.postTmpHumData();
+  Serial.println("-------- Task done --------");
 }
 
 void loop() {
-  loopSleep(callback, 2);
+  HC_utils::deepSleepLoop(callback, sleepInterval);
 }
+
+
+
+// #define ledPin 2 /**/
+
+// void setup()
+// {
+//   pinMode(ledPin, OUTPUT);
+//   digitalWrite(ledPin, HIGH);
+
+// }
+
+// void loop()
+// {
+//   digitalWrite(ledPin, LOW);
+//   delay(5000);
+//   ESP.deepSleep(10e6); /* Sleep for 5 seconds */
+// }
